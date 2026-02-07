58bb061085689159fa805b56b46c55f727f0ec09
RakhiJangir <rakhijangir54@gmail.com>
2026-02-07 10:56:41 +0530

    Update Music_Store_Analysis_Query.sql

--- Music_Store_Analysis_Query.sql
+++ Music_Store_Analysis_Query.sql
@@ -6,3 +6,11 @@ from employee
 order by levels 
 limit 1;
 
+/* Which countries have the most invoices? */
+
+select billing_counry, count(*) as total_invoice 
+from invoice 
+group by billing_country 
+order by total_invoice 
+desc;
+
