1f757cbc512c745d512a0806248e06dd12c8f602
RakhiJangir <rakhijangir54@gmail.com>
2026-02-07 11:02:43 +0530

    Update Music_Store_Analysis_Query.sql

--- Music_Store_Analysis_Query.sql
+++ Music_Store_Analysis_Query.sql
@@ -14,3 +14,15 @@ group by billing_country
 order by total_invoice 
 desc;
 
+
+/* What are top 3 values of total invoices? */
+
+Select total from invoice 
+order by total 
+desc limit 3;
+
+
+/* Which city has the best customer? We would like to through a promotional music festival in the city we made the most money. 
+Write a query that return one city that have the highest sum of invoice totals. Return both city name and sum of total invoice. */
+
+
